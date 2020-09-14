7966e740317d211fe13ad515d71e803aa6e58db5
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-09-15 00:37:04 +0530

    deep dive into procs and lambdas

--- procs_and_lambdas.rb
+++ procs_and_lambdas.rb
@@ -1,11 +1,52 @@
-new_proc = Proc.new { |var| puts var * var }
+# new_proc = Proc.new { |var| puts var * var }
 
-# Examin Proc Object 
-p new_proc.class
-p new_proc.class.superclass
-p new_proc.class.ancestors
+# # Examin Proc Object 
+# p new_proc.class
+# p new_proc.class.superclass
+# p new_proc.class.ancestors
 
-#calling Proc 
+# unable to assign blocks to variables with out proc declaration
+# some_block = { puts 'hello world' }
+# some_block.call
 
-new_proc.call(5)
-new_proc.call(10)
+# #calling Proc 
+
+# new_proc.call(5)
+# new_proc.call(10)
+
+# #creating lambda in one way
+# new_lamda_one = lambda { puts "executing from lambda one" }
+
+# #creating lambda in other way
+# new_lambda_two = -> { puts "executing from lambda two" }
+
+# new_lamda_one.call
+# new_lambda_two.call
+
+
+
+# # Differences Between Proc and Lambda
+
+# # 1. Proc return last evaluate return value, Lambda first return value
+
+
+first_proc = Proc.new { |num| 
+    num * num
+    puts "Overriding generic return statement"
+    return "Over rided written value"
+}
+
+
+first_block = ->(num) {
+    return num * num
+    puts "Overriding generic return statement"
+    return "Over rided written value"
+}
+
+p first_proc.class.ancestors
+p first_block.class.ancestors
+
+puts first_proc.call(5)
+puts first_block.call(10)
+
+
