8cad293d4630a82bef3aaf6a3bbb2470055775a4
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-09-14 16:49:45 +0530

    examin blocks in detail

--- explore_blocks.rb
+++ explore_blocks.rb
@@ -1,7 +1,21 @@
-def full_names
-    names = []
-    1.upto(10) { |i| names << yield("firstName#{i}", "lastName#{i}") }
-    names
+def some_method
+    yield(5) if block_given?
 end
 
-puts full_names { |first, last| "#{first} #{last}" }
+def other_method(&block)
+    p block.class
+    #block.call
+    block.call('yugesh')
+end
+
+#calling method without giving block
+some_method
+
+#calling method with block given
+
+some_method { |num| puts "Block Is Called with argument - #{num}" }
+
+
+#calling other_method
+
+other_method { |word| puts "reverse of given word - #{word.reverse}" }
