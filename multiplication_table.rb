3f9af21ab6743a32fd4f7bc1eda6a9ba70250ed4
Yugesh Palvai <yugeshpalvai21@gmail.com>
2020-09-30 14:07:56 +0530

    Ruby Code Challenge - Multiplication Table

---
+++ multiplication_table.rb
@@ -0,0 +1,17 @@
+def multiplication_table number
+    hash = {}
+
+    1.upto(number) do |num|
+        num_table = []
+        1.upto(10) do |digit|
+            num_table << digit * num
+        end
+        hash[num] = num_table
+    end
+
+    hash
+end
+
+table =  multiplication_table 3
+
+p table
