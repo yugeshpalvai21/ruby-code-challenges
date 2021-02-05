40b04caacc0164dbb8855857e106b289d7b19a2f
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-02-05 12:10:50 +0530

    Ruby Coding Challenge - Generate Players Bar Names

---
+++ generate_player_bar.rb
@@ -0,0 +1,14 @@
+players = File.open("players_list.txt").read.split("\n")
+
+last_names = players.map { |player| player.split(" ").last}
+
+puts players.inspect
+puts last_names.inspect
+
+bars = last_names.map.with_index do |last, index|
+    last_names.select { |name| name == last }.count > 1 ? "#{players[index][0]}. #{last}": "#{last}"
+end
+
+puts bars.inspect
+
+File.open("player_bar_names.txt", "w") { |file| file.write bars.join("\n") }
