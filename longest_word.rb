9e25972edbc08e732fab1d1ad40f9c6a96621eb2
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-02-16 13:49:41 +0530

    Ruby Coding Challenge - Longest word in given string using inject/reduce

---
+++ longest_word.rb
@@ -0,0 +1,8 @@
+def longest_word str
+    # str.split(" ").inject("") { |accum, word| accum.length > word.length ? accum : word }    
+    str.split(" ").reduce("") { |accum, word| accum.length > word.length ? accum : word }    
+end
+
+str = 'The quick brown fox jumped over the lazy dog'
+
+puts longest_word str
