05e2bb4d7208b0d6abb1399966d21e38c5a26ed1
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-03-22 12:18:07 +0530

    deep dive into bundle evaluation process

---
+++ bundler_deep_dive/rubylib1.rb
@@ -0,0 +1,11 @@
+class Person
+    def initialize
+        @name = 'yugesh reddy palvai'
+    end
+
+    def name
+        @name
+    end
+end
+
+person = Person.new
---
+++ bundler_deep_dive/rubylib2.rb
@@ -0,0 +1,14 @@
+require_relative 'rubylib1'
+
+# puts person.inspect
+# puts person.name
+
+# puts $LOADED_FEATURES.inspect
+# path = $LOAD_PATH.find do |path|
+#     File.exist?(File.join(path, 'devise-4.7.3'))
+# end
+
+
+puts $LOAD_PATH
+
+
