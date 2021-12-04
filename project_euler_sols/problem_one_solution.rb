5f7be367dffd435fe8a1e6cbdf3e5583e2455fc8
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-12-04 08:21:59 +0530

    project euler - Problem One Solution

---
+++ project_euler_sols/problem_one_solution.rb
@@ -0,0 +1,16 @@
+### PROBLEM ###
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+p [*1...1000].select { |num| (num%3 == 0) || (num%5 == 0) }.reduce { |sum, num| sum + num }
+
+# simplied like below
+
+p [*1...1000].select { |num| (num%3 == 0) || (num%5 == 0) }.reduce(&:+)
+
+# simplied like below
+
+p [*1...1000].select { |num| (num%3 == 0) || (num%5 == 0) }.sum
