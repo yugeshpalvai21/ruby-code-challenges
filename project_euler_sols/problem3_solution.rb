82f7a063b2bd051d7b8b53d3cefa955f952bbfa2
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-12-05 16:28:43 +0530

    project euler - problem three updated solution

--- project_euler_sols/problem3_solution.rb
+++ project_euler_sols/problem3_solution.rb
@@ -6,10 +6,23 @@
 # STEP2 - Find Prime Factores 
 # STEP3 - Find Largest Number
 
-require 'prime'
-
-def largest_prime_factor number
-  Prime.prime_division(number).last[0]
+def prime? number
+  factores = [*2...(number/2).floor].select { |factor| number%factor == 0 }
+  factores.empty? ? true : false
 end
 
-p largest_prime_factor 600851475143
+def largest_prime_factor number
+  largest_factor = 0
+  counter = 3
+  
+  while counter <= number
+    if (number%counter == 0) && (prime? counter)
+      largest_factor = counter
+      number = number/counter
+    end
+    counter += 1
+  end
+  return largest_factor
+end
+
+p largest_prime_factor 600_851_475_143
