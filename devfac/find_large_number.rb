743ac989dd02439afdb03def0b713622d29a7501
yugeshpalvai <yugeshpalvai21@gmail.com>
2022-03-13 21:58:29 +0530

    benchmarking different ways to find max number

--- devfac/find_large_number.rb
+++ devfac/find_large_number.rb
@@ -1,10 +1,46 @@
-def find_large_number arr
-  arr.inject { |a, v|  a > v ? a : v }
+require 'benchmark'
+
+def new_array
+  Array.new(1_000_000_00) { rand 1000 }
 end
 
-RSpec.describe "#find_large_number" do
-  it 'will returns large number in given array' do
-    array_of_numbers = [10, 23, 5, 34, 3]
-    expect(find_large_number(array_of_numbers)).to eq 34
-  end
+def find_large_number_one
+  arr = new_array
+  p arr.max
 end
+
+
+def find_large_number_two
+  arr = new_array
+  p arr.inject { |a, v|  a > v ? a : v }
+end
+
+def find_large_number_three
+  arr = new_array
+  p arr.sort.last
+end
+
+def find_large_number_four
+  arr = new_array
+  max_value = 0
+  arr.each do |val|
+    if val > max_value
+      max_value = val
+    end
+  end
+  p max_value
+end
+
+Benchmark.bm do |x|
+  x.report("WMAX - ") { find_large_number_one }
+  x.report("INJECT - ") { find_large_number_one }
+  x.report("SORT - ") { find_large_number_one }
+  x.report("EACH - ") { find_large_number_one }
+end
+
+# RSpec.describe "#find_large_number" do
+#   it 'will returns large number in given array' do
+#     array_of_numbers = [10, 23, 5, 34, 3]
+#     expect(find_large_number(array_of_numbers)).to eq 34
+#   end
+# end
