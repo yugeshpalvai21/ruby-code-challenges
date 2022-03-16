e4ea8adae54f5a3c9bd500fd78eec79be765305b
yugeshpalvai <yugeshpalvai21@gmail.com>
2022-03-16 21:22:56 +0530

    hash to parameter

---
+++ devfac/hash_to_parameter.rb
@@ -0,0 +1,13 @@
+def hash_converter data
+  data.map { |k, v| "#{k}=#{v}" }.join("&")
+  # result = ""
+  # data.each { |k,v| result += "#{k}=#{v}&"}
+  # result[0...-1]
+end
+
+RSpec.describe "#hash_converter" do
+  it 'will converts hash to url equilant parameter' do
+    hash = { name: 'yugesh', location: 'ind' }
+    expect(hash_converter(hash)).to eq("name=yugesh&location=ind")
+  end
+end
