4e46bfd952ae61b299401364cd3a092369db7ae6
yugeshpalvai <yugeshpalvai21@gmail.com>
2022-03-16 21:29:44 +0530

    optimized solution - hash to param converter

--- devfac/hash_to_parameter.rb
+++ devfac/hash_to_parameter.rb
@@ -1,9 +1,14 @@
 def hash_converter data
-  data.map { |k, v| "#{k}=#{v}" }.join("&")
-  # result = ""
-  # data.each { |k,v| result += "#{k}=#{v}&"}
-  # result[0...-1]
+  data.map { |i| i * "=" } * "&" 
 end
+
+
+# def hash_converter data
+#   data.map { |k, v| "#{k}=#{v}" }.join("&")
+#   # result = ""
+#   # data.each { |k,v| result += "#{k}=#{v}&"}
+#   # result[0...-1]
+# end
 
 RSpec.describe "#hash_converter" do
   it 'will converts hash to url equilant parameter' do
