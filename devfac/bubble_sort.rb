81a64a2d05773a5fb143a306b8e62a816329ce7f
yugeshpalvai <yugeshpalvai21@gmail.com>
2022-03-17 21:29:33 +0530

    solved - bubble sort

--- devfac/bubble_sort.rb
+++ devfac/bubble_sort.rb
@@ -1,6 +1,25 @@
+class Array
+  def bubble_sort
+    arr_length = self.length
+    loop do
+      sorted = false
+      (arr_length-1).times do |i|  #we don't need to compare last element
+        if self[i] > self[i+1]
+          self[i], self[i+1] = self[i+1], self[i]
+          sorted = true
+        end
+      end
+      break unless sorted
+    end
+    self
+  end
+end
+
 RSpec.describe Array do
   describe '#bubble_sort' do
-    test_arr = [3,5,2,10,6]
-    expect(test_arr.bubble_sort).to eq(test_arr.sort)
+    it 'will sort given array elements' do
+      test_arr = [3,5,2,10,6]
+      expect(test_arr.bubble_sort).to eq(test_arr.sort)
+    end
   end
 end
