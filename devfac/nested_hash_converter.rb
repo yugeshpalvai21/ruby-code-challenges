892a8af3e7a74bcf034ae1665e0a298abd184812
yugeshpalvai <yugeshpalvai21@gmail.com>
2022-03-19 16:26:25 +0530

    code challenge - nested hash converter

--- devfac/nested_hash_converter.rb
+++ devfac/nested_hash_converter.rb
@@ -1,3 +1,15 @@
+def nested_hash_converter hash
+  hash.each_with_object({}) do |(k, v), h|
+    if v.is_a? Hash
+      nested_hash_converter(v).map do |h_k, h_v|
+        h["#{k}_#{h_k}".to_sym] = h_v
+      end
+    else 
+      h[k] = v
+    end
+   end
+end
+
 RSpec.describe '#nested_hash_converter' do
   it 'wii converts nested hash keys to single hash' do
     base_hash = { title: 'yugesh', location: { base_details: { city: 'hyd' }}}
