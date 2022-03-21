c020d081f3a187b3c1418b8322ff43e0ec83135a
yugeshpalvai <yugeshpalvai21@gmail.com>
2022-03-21 19:25:54 +0530

    div_generator - string interpolation with percentage

--- devfac/div_generator.rb
+++ devfac/div_generator.rb
@@ -1,7 +1,10 @@
 def content_for data
-  open_tag = "<div>"
-  close_tag = "</div>"
-  "#{open_tag}#{data}#{close_tag}"
+  streamed_data = %w(div data div)
+  streamed_data[1] = data.to_s
+  "<%s>%s</%s>" % streamed_data
+  # open_tag = "<div>"
+  # close_tag = "</div>"
+  # "#{open_tag}#{data}#{close_tag}"
 end
 
 RSpec.describe "#content_for" do
