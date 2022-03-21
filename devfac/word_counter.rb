db014d60c14570b6a511cca94b6967e04a579d8b
yugeshpalvai <yugeshpalvai21@gmail.com>
2022-03-21 20:00:05 +0530

    refactoring code to acheive DRY

--- devfac/word_counter.rb
+++ devfac/word_counter.rb
@@ -1,25 +1,30 @@
 class String
+  def extract_words
+    self.scan(/\w+[a-zA-Z]/)
+  end
+
   def word_count
-    self.scan(/\w+[a-zA-Z]/).length
+    extract_words.length
   end
 
   def word_list
     result = Hash.new(0)
-    self.scan(/\w+[a-zA-Z]/).each { |word| result[word.to_sym] += 1 }
+    extract_words.each { |word| result[word.to_sym] += 1 }
     result
   end
 end
 
 RSpec.describe String do
+  let(:str) { "- the quick brown fox / jumped over 45 the lazy fox." }
+
   describe "#word_count" do
     it 'will counts number of occurences of english words' do
-      str = "- the quick brown fox / jumped over 45 the lazy fox."
       expect(str.word_count).to eq 9
     end
   end
+
   describe "#word_list" do
     it 'will counts number of occurences of each word' do
-      str = "- the quick brown fox / jumped over 45 the lazy fox."
       expect(str.word_list).to eq({the: 2, 
                                     quick: 1,
                                     brown: 1,
