17a1a3e0dcdf00c4402ea65cbc1cfab077f198ac
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-07-20 23:52:08 +0530

    Revisits logic for langest word in given sentence

--- 01_longest_word.rb
+++ 01_longest_word.rb
@@ -1,8 +1,9 @@
-def longest_word str
+# Programm that prints logest word in given sentence
+def longest_word sentence
     # str.split(" ").inject("") { |accum, word| accum.length > word.length ? accum : word }
-    str.split.reduce { |accum, word| accum.length > word.length ? accum : word }
+    sentence.split.reduce { |accum, word| accum.length > word.length ? accum : word }
 end
 
-str = 'The quick brown fox jumped over the lazy dog'
+sentence = 'The quick brown fox jumped over the lazy dog'
 
-puts longest_word str
+puts longest_word sentence
