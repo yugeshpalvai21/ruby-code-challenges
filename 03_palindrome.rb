fb30a82bcd4a6964c5f9121222a1da9fa9feff62
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-07-22 20:07:38 +0530

    Revisits logic for palindrome  and refactored existing logic

--- 03_palindrome.rb
+++ 03_palindrome.rb
@@ -1,7 +1,10 @@
+# programm on checking weather given string is palindrome or not
+# A palindrome is a word, number, or phrase that reads the same backward and forward, such as “civic” or “2002.”
 def palindrome? str
-    str.downcase == str.reverse.downcase
+    str.to_s.downcase == str.to_s.reverse.downcase
 end
 
 puts palindrome? 'hello'
 puts palindrome? 'tacocat'
 puts palindrome? 'TacoCat'
+puts palindrome? 2002
