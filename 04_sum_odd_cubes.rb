4fdcb95c8be443386960b72a9435373e39a7edb5
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-07-22 20:24:08 +0530

    Revisits logic for cubucal numbers  and refactored existing logic

--- 04_sum_odd_cubes.rb
+++ 04_sum_odd_cubes.rb
@@ -1,9 +1,13 @@
 
 def total_sum_for_cube_of_odd_numbers numbers
-    numbers.select { |num| num%2 == 1 }.map { |num| num**3 }.inject(:+)
+    # Step-1: for any given array of numbers, find out odd numbers
+    # Step-2: iterate through each odd number and evaluate cubical of it
+    # Step-3: sum up all the numbers from above array
+    # This logic illustrates concept of method chaining
+    numbers.select { |num| (num.to_i)%2 == 1 }.map { |num| num**3 }.inject(:+)
 end
 
-numbers = [2,5,6,8,4,7,13,17]
+numbers = [2,5,6,8,4,7,13,17, "dsfsdf"]
 
 result = total_sum_for_cube_of_odd_numbers numbers
-puts result.inspect
+puts result
