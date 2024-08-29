bed2bbf6e88aaba277d06a2dee98930be0747906
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-08-29 18:40:10 +0530

    BowlingGame: implements scoring for gutter game

--- 14_ruby_with_rspec/lib/bowling_game.rb
+++ 14_ruby_with_rspec/lib/bowling_game.rb
@@ -1,2 +1,13 @@
 class BowlingGame
+  def initialize
+    @rolls = []
+  end
+
+  def roll(pins)
+    @rolls << pins
+  end
+
+  def score
+    @rolls.sum
+  end
 end
--- 14_ruby_with_rspec/spec/bowling_game_spec.rb
+++ 14_ruby_with_rspec/spec/bowling_game_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe BowlingGame do
   it 'instantiate BowlingGame object' do
     expect(subject).to be_instance_of(BowlingGame)
   end
+
+  it 'can score a gutter game (all 0s)' do
+    20.times { subject.roll(0) }
+    expect(subject.score).to eq(0)
+  end
 end 
