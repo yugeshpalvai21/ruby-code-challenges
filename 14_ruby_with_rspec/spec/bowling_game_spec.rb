90d4cf3254726c150ab05309df02197ce89f8131
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-08-29 18:46:06 +0530

    BowlingGame: implements for a game of all ones

--- 14_ruby_with_rspec/spec/bowling_game_spec.rb
+++ 14_ruby_with_rspec/spec/bowling_game_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe BowlingGame do
     20.times { subject.roll(0) }
     expect(subject.score).to eq(0)
   end
+
+  it 'can score a game of all ones' do
+    20.times { subject.roll(1) }
+    expect(subject.score).to eq(20)
+  end
 end 
